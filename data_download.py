5c6f8248d304a4904e900ace9e4c59d7f9df038b
Денис <usakovdenis440@gmail.com>
2024-10-30 09:57:10 +0400

    Update data_download.py

--- data_download.py
+++ data_download.py
@@ -18,3 +18,19 @@ def calculate_and_display_average_price(data):
     logging.info(f'Выводится среднее значение колонки "Close": {average_price}')
     print(f'Среднее значение колонки "Close": {average_price}\n')
     return data
+
+def notify_if_strong_fluctuations(data, threshold):
+    """ Уведомляет пользователя, если цена акций колебалась более чем на заданный процент за период. """
+    list_prices_close = data['Close'].tolist()
+    max_price = max(list_prices_close)
+    min_price = min(list_prices_close)
+    difference = max_price - min_price
+    """Средняя цена и порог колебания цен в процентах:"""
+    average_price = (max_price + min_price) / 2
+    threshold = difference / (average_price / 100)
+    """Разница между максимальной и минимальной ценой в процентах."""
+    percentage_difference = difference / min_price * 100
+
+    if percentage_difference > threshold:
+        logging.info(f'Значение колебаний: {percentage_difference}')
+        print(f'Превышен порог цен -{percentage_difference}, допустимое значение -{threshold}')
